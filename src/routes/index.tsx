e6f0b9e07f0c153f573e359810940b5909d16675
刘臻 <liuzhen6@longcheer.com>
2025-07-04 17:58:53 +0800

    Add DaisyUI for enhanced styling and refactor route component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,38 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { useState } from 'react'
+import reactLogo from '../assets/react.svg'
+import viteLogo from '/vite.svg'
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div className='text-red-500'>Hello "/"!</div>
+    const [count, setCount] = useState(0)
+  
+  return (
+    <div className='flex flex-col items-center'>
+      <div className='flex justify-center gap-2'>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button className='btn' onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
 }
+
