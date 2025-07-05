8b9738ecb1321647dafaddf8b36cd059fb43b75b
zhen <770226915@qq.com>
2025-07-05 22:50:40 +0800

    feat: 添加tailwind配置并优化页面样式
    - 调整页面布局和间距，使用tailwind实用类
    - 为React logo添加旋转动画效果
    - 移除未使用的提交按钮

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,26 +11,25 @@ function RouteComponent() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="flex flex-col items-center">
-      <div className="flex justify-center gap-2">
+    <div className="mt-5 flex flex-col items-center gap-3">
+      <div className="flex h-48 w-48 justify-center gap-4">
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="size-full" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="size-full motion-safe:animate-spin" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button className="btn" onClick={() => setCount(count => count + 1)}>
+        <button className="btn mb-2" onClick={() => setCount(count => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>routes/index.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">Submit</button>
     </div>
   );
 }
