4a680a57b1e619c09146aff67b6c5e87570a7f66
刘臻 <liuzhen6@longcheer.com>
2025-11-07 16:34:22 +0800

    feat(header): 添加滚动监听以动态控制阴影显示
    在 Header 组件中引入 useEffect 监听页面滚动事件，根据滚动偏移量动态添加阴影效果。
    同时优化了组件的类名结构与部分样式细节，提升视觉表现与性能体验。

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/utils";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Separator } from "./base/separator";
 import { SidebarTrigger } from "./ui/sidebar";
 
@@ -10,17 +10,31 @@ type HeaderProps = React.HTMLAttributes<HTMLElement> & {
 export default function Header({ className, fixed, children, ...props }: HeaderProps) {
   const [offset, setOffset] = useState(0)
 
+  useEffect(() => {
+    const onScroll = () => {
+      setOffset(document.body.scrollTop || document.documentElement.scrollTop)
+    }
+    // 将 passive 设为 true 可以启用性能优化，并可大幅改善应用性能
+    document.addEventListener('scroll', onScroll, { passive: true })
+
+    return () => document.removeEventListener("scroll", onScroll)
+  }, [])
+
   return (
-    <header className={cn(
-      "z-10 h-16",
-      fixed && "sticky top-0 w-[inherit]",
-      offset > 0 && fixed ? "shadow" : "shadow-none",
-      className,
-    )}>
+    <header
+      className={cn(
+        "z-10 h-16",
+        fixed && "sticky top-0 w-[inherit]",
+        offset > 10 && fixed ? "shadow" : "shadow-none",
+        className,
+      )}
+      {...props}
+    >
       <div className={cn(
-        "flex h-full items-center gap-3 p-4 sm:gap-4"
+        "flex h-full items-center gap-3 p-4 sm:gap-4 relative",
+        fixed && "after:bg-background/20 after:absolute after:inset-0 after:-z-10 after:backdrop-blur-lg"
       )}>
-        <SidebarTrigger variant={"outline"} className="max-md:scale-125"/>
+        <SidebarTrigger variant={"outline"} className="max-md:scale-125" />
         <Separator className={"h-6"} orientation="vertical" />
         {children}
       </div>
